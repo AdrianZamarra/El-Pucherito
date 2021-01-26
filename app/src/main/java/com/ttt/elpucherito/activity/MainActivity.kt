eb9d069085813709ad0569e3b27dfe3e1e1d450c
ThePelican18 <danielnj18@gmail.com>
2021-01-26 13:46:38 +0100

    Añadido el paquete activity y bd

--- app/src/main/java/com/ttt/elpucherito/activity/MainActivity.kt
+++ app/src/main/java/com/ttt/elpucherito/activity/MainActivity.kt
@@ -1,12 +1,16 @@
-package com.ttt.elpucherito
+package com.ttt.elpucherito.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.ttt.elpucherito.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
