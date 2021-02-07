eb66a5961cc0390e97a968ed60e6546ac36a9b99
Adrian <adrizamarra@gmail.com>
2021-02-07 13:58:52 +0100

    conflictos

--- app/src/main/java/com/ttt/elpucherito/activity/MainActivity.kt
+++ app/src/main/java/com/ttt/elpucherito/activity/MainActivity.kt
@@ -12,31 +12,11 @@ import kotlinx.coroutines.launch
 import java.util.*
 import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity(), CoroutineScope {
-
-    override val coroutineContext: CoroutineContext
-        get() = Dispatchers.Main
+class MainActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val thread = Thread {
-            var db: ElPucheritoDB = ElPucheritoDB.getInstance(this)
-            println("${Thread.currentThread()} has run.")
-            var user : User = User(null,"Adri","Mazarra","Calle la sierra",123123132,"orangu@gmail.com","a")
-
-            launch {
-
-                db.userDao().insertUser(user)
-            }
-
-            val messageList: List<User> =  db.userDao().getUsers()
-            println("asdasads->>>> " + messageList)
-        }
-        thread.start()
-
-
 
     }
 
