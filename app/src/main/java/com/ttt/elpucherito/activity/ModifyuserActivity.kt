7de74311a63f0c4bca1dd6aea7c27eaf1d1ac857
Adrian <adrizamarra@gmail.com>
2021-02-07 23:18:15 +0100

    conflictos

---
+++ app/src/main/java/com/ttt/elpucherito/activity/ModifyuserActivity.kt
@@ -0,0 +1,26 @@
+package com.ttt.elpucherito.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.drawerlayout.widget.DrawerLayout
+import com.ttt.elpucherito.R
+
+class ModifyuserActivity : AppCompatActivity() {
+
+    var drawerLayout : DrawerLayout? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modifyuser)
+
+
+
+
+
+
+
+
+    }
+}
